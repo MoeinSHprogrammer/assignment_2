bd949f5adc4bf38cbfd6d40dae8b9aa04fd3fcc9
MoeinSHprogrammer <111762145+MoeinSHprogrammer@users.noreply.github.com>
2023-09-21 16:48:14 +0330

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,10 @@
+
+var = 0
+
+while(1):
+    inp = input()
+    if inp == 'exit':
+        break
+    else:
+        var += int(inp)
+print(var)
---
+++ Ex4.py
@@ -0,0 +1,18 @@
+
+n = int(input("number of student"))
+
+var = 0
+maxx = 0
+minn = 20
+
+for i in range (n):
+    p = float(input())
+    max
+    if  p > maxx:
+        maxx = p
+    if p < minn:
+        minn = p
+    var += p
+print("mean is: ",var / n)
+print("max is: ",maxx)
+print("min is: ",minn)
